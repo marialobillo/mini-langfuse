e52f42ffdf6593ca1f945ecba84b13adcf3155b8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2026-05-10 19:53:17 +0200

    feat: add transparent trace decorator

--- packages/sdk/tests/test_trace.py
+++ packages/sdk/tests/test_trace.py
@@ -2,8 +2,16 @@ from mini_langfuse_sdk import trace
 
 
 def test_decorated_function_returns_same_value():
-
     original_func = lambda: 12
     traced_func = trace(original_func)
-    
-    assert traced_func() == original_func()
+    assert traced_func() == 12
+
+def test_decorated_function_works_with_positional_arguments():
+    original_func = lambda a, b: a * b 
+    traced_func = trace(original_func)
+    assert traced_func(2,3) == 6
+
+def test_decorated_function_works_with_keyword_arguments():
+    original_func = lambda a, b: a * b
+    traced_func = trace(original_func)
+    assert traced_func(a=2,b=3) == 6
